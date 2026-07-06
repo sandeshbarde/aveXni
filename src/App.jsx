c46c37e48875b4226c85cc6dbf1a907b0e4b8d7f
Sandesh Barde <sandeshvitthal375@gamil.com>
2026-07-07 01:30:03 +0700

    Test GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-
-import {
-  ThemeProvider,
-  AuthProvider,
-  CartProvider,
-  OrderProvider,
-  GalleryProvider,
-  NotificationProvider,
-  AppProvider,
-} from "./context";
-
-import "./assets/styles/variables.css";
-import "./assets/styles/globals.css";
-import "./assets/styles/animations.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <AuthProvider>
-          <CartProvider>
-            <OrderProvider>
-              <GalleryProvider>
-                <NotificationProvider>
-                  <AppProvider>
-                    <AppRoutes />
-                  </AppProvider>
-                </NotificationProvider>
-              </GalleryProvider>
-            </OrderProvider>
-          </CartProvider>
-        </AuthProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <HashRouter>
+      <AppRoutes />
+    </HashRouter>
   );
 }
