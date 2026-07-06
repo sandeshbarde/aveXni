b48c8832048f116983add667440c9c8c9de1cf66
Sandesh Barde <sandeshvitthal375@gamil.com>
2026-07-06 23:48:58 +0700

    Add GitHub Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
+  // GitHub repository name
+  base: "/aveXni/",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
